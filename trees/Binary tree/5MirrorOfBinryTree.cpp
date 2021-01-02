f7fc2dc71a04536f42f59f20bfb8ab2ca4b3486f
GITANJALI SAINI <sgitanjali20@gmail.com>
2021-01-02 13:53:10 +0530

    updated

---
+++ trees/Binary tree/5MirrorOfBinryTree.cpp
@@ -0,0 +1,49 @@
+//problem link : https://leetcode.com/problems/invert-binary-tree/
+/* Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) 
+    {
+        TreeNode *temp;
+        if(root== NULL)
+            return root;
+        else
+        {
+            invertTree(root->left);
+            invertTree(root->right);
+
+            temp=root->left;
+            root->left=root->right;
+            root->right=temp;
+            return root;
+        } 
+    }
+};
