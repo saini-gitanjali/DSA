34c8fceba706b04208f38def5c2a48303ba1b935
GITANJALI SAINI <sgitanjali20@gmail.com>
2021-01-03 12:13:27 +0530

    updated

--- STACKS AND QUEUES/5NstacksInSingleArray.cpp
+++ STACKS AND QUEUES/5NstacksInSingleArray.cpp
@@ -1,3 +1,7 @@
+//Create a data structure kStacks that represents k stacks. Implementation of kStacks should use only one array, i.e., k stacks should use the same array for storing elements. Following functions must be supported by kStacks.
+
+//push(int x, int k) –> pushes x to stack number ‘sn’ where sn is from 0 to k-1
+//pop(int k) –> pops an element from stack number ‘sn’ where sn is from 0 to k-1
 #include<iostream>
 #include<stdio.h.>
 #include<string.h>
@@ -12,80 +16,80 @@ class Nstack{
     int no_of_stacks;
 
     public:
-    Nstack(int n, int k);
-    void push(int n, int k);
-    int pop(int k);
-    int peek(int k);
-    bool isfull(int k);
-    bool isempty(int k);
+    Nstack(int n, int sn);
+    void push(int n, int sn);
+    int pop(int sn);
+    int peek(int sn);
+    bool isfull(int sn);
+    bool isempty(int sn);
 
-// uncomment this function to see min and max value of top for each stack
-   /* void utility(int n, int k){
-        for(int i=0; i<k ; i++)
+// uncomment this function to see min and max value of top for each stacsn
+   /* void utility(int n, int sn){
+        for(int i=0; i<sn ; i++)
         cout<<top[i]<<"\t"<<sizeStack[i]<<endl;
     }*/
 };
 
-Nstack :: Nstack(int n, int k)
+Nstack :: Nstack(int n, int sn)
 {
     size = n;
-    no_of_stacks = k;
+    no_of_stacks =sn;
     stack = new int[n];
-    top = new int[k];
-    sizeStack = new int[k];
+    top = new int[sn];
+    sizeStack = new int[sn];
     top[0] = -1;
-    for(int i=1; i<k; i++)
-        top[i] = top[i-1] + (n/k);
-    for(int i=0; i<k; i++)
-        sizeStack[i] = top[i]+k-1;
+    for(int i=1; i<sn; i++)
+        top[i] = top[i-1] + (n/sn);
+    for(int i=0; i<sn; i++)
+        sizeStack[i] = top[i]+sn-1;
     //stack ={0};
 }
 
-bool Nstack :: isfull(int k){
-    if(top[k-1] == sizeStack[k-1])
+bool Nstack :: isfull(int sn){
+    if(top[sn-1] == sizeStack[sn-1])
         return true;
     else 
         return false;
 }
 
-bool Nstack ::isempty(int k){
-    if(k == 1)
+bool Nstack ::isempty(int sn){
+    if(sn == 1)
         if(top[0]==-1)
         {
             // cout<<top[0];
         return true;
         }
-    else if(top[k-1] == sizeStack[k-2]-1)
+    else if(top[sn-1] == sizeStack[sn-2]-1)
         return true;
     else
         return false;
 }
 
-void Nstack :: push(int n, int k){
-    if(isfull(k))
+void Nstack :: push(int n, int sn){
+    if(isfull(sn))
     {
         cout<<"OverFlow";
         return;
     }
-    stack[++top[k-1]] = n;
-    cout<<top[k-1]<<" "<<stack[top[k-1]];
+    stack[++top[sn-1]] = n;
+    //cout<<top[sn-1]<<" "<<stack[top[sn-1]];
 }
 
-int Nstack :: peek(int k){
+int Nstack :: peek(int sn){
     
-    if(!isempty(k))
-        return stack[top[k-1]];
+    if(!isempty(sn))
+        return stack[top[sn-1]];
         
     else
         cout<<"underflow";
 }
 
-int Nstack :: pop(int k)
+int Nstack :: pop(int sn)
 {
-    if(!isempty(k))
+    if(!isempty(sn))
     {
-        int t = stack[top[k-1]--];
-        cout<<top[k-1]<<endl;
+        int t = stack[top[sn-1]--];
+        cout<<top[sn-1]<<endl;
         return t;
     }
     else
@@ -93,14 +97,14 @@ int Nstack :: pop(int k)
 }
 
 int main(){
-    int n, k , m;
+    int n, sn , m;
     cout<<"enter stack size : ";
-    cin>>n>>k;
+    cin>>n>>sn;
     
     int choice1=0, choice2=0;
-    Nstack st(n, k);
+    Nstack st(n, sn);
     //uncomment the below function to see min and max values of taop for each stack
-    //st.utility(n, k);
+    //st.utility(n, sn);
     do {
             cout<<"\n1.push";
             cout<<"\n2.pop";
