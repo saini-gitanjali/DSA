82745311946c867b10ea878580d90499879d0ee6
GITANJALI SAINI <sgitanjali20@gmail.com>
2021-01-29 16:45:43 +0530

    updated

--- GRAPHS/bellmanFordAlgo.cpp
+++ GRAPHS/bellmanFordAlgo.cpp
@@ -17,36 +17,9 @@ void addEdge(int src, int dest, int weight, bool bidirectional = true)
         adjList[dest].push_back(make_pair(src, weight));
 }
 
-void dijkstra(int V, int src)
+void bellmanFord(int V, int src)
 {
-    //first element will be distance as elements will be sorted according to fist element of pair
-    for(auto j: adjList) 
-        dist[j.first] = INT16_MAX;
-    set<pair<int, int> > s;         //set to find\ node at a min distance from source
-    // initialization
-    dist[src] = 0;      //distance of any node from itself is zero
-    s.insert(make_pair(0, src)); 
-    while(!s.empty()) {
-        auto curr = *(s.begin());
-        int node = curr.second;
-        int currDist = curr.first;
-        s.erase(s.begin());
-        for(auto child: adjList[node]) {
-            if(currDist + child.second < dist[child.first]) {
-                int temp = child.first;
-                auto f = s.find(make_pair(dist[temp], temp));
-                if(f != s.end()) {
-                    s.erase(f);
-                }
-                dist[temp] = child.second+currDist;
-                s.insert(make_pair(dist[temp], temp));
-            }
-        } 
-    }
-    for (auto d : dist)
-        {
-            cout<<d.first<<"\t"<<d.second<<endl; 
-        }
+    
 }
 
 void print(int V) {  
@@ -59,7 +32,6 @@ void print(int V) {
 }
 
 int main(){
-    
     int V=10000,E, w, s, d;
     cin>>V>>E;    
     bool bidirectional;
@@ -75,6 +47,6 @@ int main(){
     int src;
     int *dist = new int[V];
         cin>>src;
-    dijkstra( V, src);
+    bellmanFord( V, src);
     return 0;
 }
