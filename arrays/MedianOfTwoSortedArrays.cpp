f8441b21b06836bbbedc83e4cc80be565c001612
GITANJALI SAINI <sgitanjali20@gmail.com>
2021-05-20 19:18:20 +0530

    solution modified

--- arrays/MedianOfTwoSortedArrays.cpp
+++ arrays/MedianOfTwoSortedArrays.cpp
@@ -7,12 +7,12 @@ using namespace std;
 int median(int *arr1, int *arr2, int n1, int n2)
 {
    int i = 0, j = 0, k = 0;
+   int size = n1+n2;
    int *arr3 =  new int[n1+n2];
     while (i < n1 && j< n2)
     {
         if(arr1[i] <= arr2[j]){
             arr3[k++] = arr1[i++];
-            cout<<"1";
         }
         else if(arr1[i] > arr2[j]){
             arr3[k++] = arr2[j++];
@@ -21,18 +21,21 @@ int median(int *arr1, int *arr2, int n1, int n2)
     while(i<n1)
         {
             arr3[k++] = arr1[i++];
-            // cout<<"98";
         }
     while(j<n2)
     {
         arr3[k++] = arr2[j++];
     }
-    cout<<endl;
-    int mid = (n1+n2)/2;
-    for(int i = 0; i<n1+n2; i++)
-        cout<<arr3[i]<<" ";
-        cout<<endl;
-    return arr3[mid];
+    int mid = size/2, ans = 0;
+    if(size%2 == 0)
+    {
+        int sum = arr3[mid] + arr3[mid - 1];
+        ans = sum/2;
+    }
+    else
+        ans = arr3[mid];
+    
+    return ans;
 }
 
 int main() {
