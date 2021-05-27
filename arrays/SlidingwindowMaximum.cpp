4718970c8617b7955d4aff1f04a39f1a58377e84
GITANJALI SAINI <sgitanjali20@gmail.com>
2021-05-27 22:50:55 +0530

    new solution added

---
+++ arrays/SlidingwindowMaximum.cpp
@@ -0,0 +1,73 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to find maximum of each subarray of size k.
+    vector <int> max_of_subarrays(int *arr, int n, int k)
+    {
+        int i;
+        vector<int> ans;
+        deque<int> q(k);
+        for(i = 0 ; i < k; i++)
+            {
+                
+                while(!q.empty() && arr[i] >= arr[q.back()])
+                {
+                    q.pop_back();
+                }
+                
+                q.push_back(i);
+            }
+            
+        for(; i < n ; i++){
+            ans.push_back(arr[q.front()]);
+            
+            while ((!q.empty()) && q.front() <= i - k)
+                q.pop_front();
+             
+             while(!q.empty() && arr[i] >= arr[q.back()])
+                {
+                    q.pop_back();
+                }
+                q.push_back(i);
+            
+        }
+        ans.push_back(arr[q.front()]);
+        return ans;
+            
+       
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin >> t;
+	
+	while(t--){
+	    
+	    int n, k;
+	    cin >> n >> k;
+	    
+	    int arr[n];
+	    for(int i = 0;i<n;i++) 
+	        cin >> arr[i];
+	    Solution ob;
+	    vector <int> res = ob.max_of_subarrays(arr, n, k);
+	    for (int i = 0; i < res.size (); i++) 
+	        cout << res[i] << " ";
+	    cout << endl;
+	    
+	}
+	
+	return 0;
+}  // } Driver Code Ends
