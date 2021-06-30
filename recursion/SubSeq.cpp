35eb2f449aefe7ce44e07896d15d5e342d34e911
GITANJALI SAINI <sgitanjali20@gmail.com>
2021-06-30 09:40:56 +0530

    new solution added

---
+++ recursion/SubSeq.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string.h>
+#include<string>
+
+using namespace std;
+
+void subseq(string str, string ans)
+{
+    if(str.length()==0){
+        cout<<ans<<endl;
+        return ;
+    }
+    char ch = str[0];
+    int code = ch;
+    string ros = str.substr(1);
+    subseq(ros, ans);
+    subseq(ros, ans+ch);
+    subseq(ros, ans + to_string(ch));
+}
+
+int main() {
+    string str;
+    cin>>str;
+    subseq(str, "");
+    return 0;
+}
